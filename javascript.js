9fd0e8338444b761b4efb00ab1dd69191c16c777
mariamabah21 <136363278+mariamabah21@users.noreply.github.com>
2023-08-03 21:14:33 +0200

    Add files via upload
    Implemented JavaScript Calculator

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
+var form = document.getElementById("formCalculator");
 
+form.addEventListener("click", function (event) {
+  {
+    console.log(event.target.value);
+  }
+});
+// If the user clicked one of the numbers or operation signs
+// just add this number or sign to the input where you show results.
